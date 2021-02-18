18df3d546e42c863aa8209d5134ed721c3bb43a6
foodmania <tobdrdoab@supere.ml>
2021-02-18 11:03:00 +0700

    tambahan search image otomatis

--- pinterest.py
+++ pinterest.py
@@ -5,9 +5,13 @@
 # @Year       : 2021                       #
 ############################################
 
+import random
 import os
+import datetime
 import svglue
 from configparser import ConfigParser
+from tqdm import tqdm
+from bing_image_urls import bing_image_urls
 
 parser = ConfigParser()
 parser.read('config.ini')
@@ -16,28 +20,50 @@ parser.read('config.ini')
 template = parser.get('settings', 'template')
 
 #option = int(input("Select a template: "))
-print("\033[1;31;40m Current Template : " + template)
-# load the template from a file
-tpl = svglue.load(file='template/' + template + '.svg')
+print("--->> Current Template : " + template)
 
-# replace some text
-tpl.set_text('sample-text', u'ngentot Goreng Klaten Utara Sangat Enak Sekali Klajo')
+# keyword line
+line_numbers = len(open('keyword.txt').readlines())
+loop = -1
 
-# to render the template, cast it to a string. this also allows passing it
-# as a parameter to set_svg() of another template
-src = str(tpl)
+# Create New Dir
+now = datetime.datetime.now()
+dateNow = now.strftime("%Y-%m-%d-%H:%M:%S")
+os.system('mkdir output/' + dateNow)
 
-# write out the result as an SVG image and render it to pdf using cairosvg
-#import cairosvg
-with open('output.svg', 'w') as svgout:
-    svgout.write(src)
-    #cairosvg.svg2pdf(bytestring=src, write_to=out)
+pbar = tqdm(total = int(line_numbers))
+while loop <= line_numbers:
+	try:
+		loop += 1
+		keyword = open('keyword.txt', 'r').readlines()
+		
+		# load the template from a file
+		tpl = svglue.load(file='template/' + template + '.svg')
 
-# export to png
-os.system('inkscape --export-type="png" output.svg')
-os.system('mv output.png output')
-os.system('rm output.svg')
+		# replace some text
+		tpl.set_text('main-title', keyword[loop])
+		
+		# replace image
+		
+		image = bing_image_urls(keyword[loop], limit=2)
+		randomImage = random.choice(image)
+		try:
+			tpl.set_image('main-img', file=randomImage, mimetype=None)
+		except:
+			tpl.set_image('main-img', file=randomImage, mimetype=None)
+		# to render the template, cast it to a string. this also allows passing it
+		# as a parameter to set_svg() of another template
+		src = str(tpl)
+		svgName = str(loop) + '.svg'
+		print(svgName)
+		with open(svgName, 'w') as svgout:
+		    svgout.write(src)
 
-
-#var list
-# svgFileName
+		# export to png
+		os.system('inkscape --export-type="png" ' + svgName)
+		os.system('mv ' + str(loop) + '.png ' + 'output/' + dateNow)
+		os.system('rm ' + svgName)
+		pbar.update(1)
+	except:
+		print("error bang, cari solusi ya")
+pbar.close()
